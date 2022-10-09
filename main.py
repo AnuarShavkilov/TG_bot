7e9ea46d1bbd957eebc675fc5463dc39b89a5e1c
anuar1910q@gmail.com <Ppoe0132es>
2022-10-09 18:54:35 +0300

    first_change

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello Tg')
+import telebot
+bot = telebot.TeleBot('5643787535:AAFxdHmSYvUXhlNRWg4kpbTlhtYBwRelzoU')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    name = f'Dattebayo, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
+    bot.send_message(message.chat.id, name, parse_mode='html')
+
+bot.polling(none_stop=True)
